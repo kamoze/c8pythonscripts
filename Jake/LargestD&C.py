7a8cfaf7c65e0548ccd1a2fbbfea016791059baa
3jaket3 <3jaket3@gmail.com>
2021-08-09 10:56:47 -0400

    D&C Algorithm to find Largest element of an array

--- Jake/LargestD&C.py
+++ Jake/LargestD&C.py
@@ -24,12 +24,16 @@ def largest(array, i: int, j: int):
 
 a = np.array([3, 9, 12, 2, 22, 25, 78, 92, 77, 10, 35, 88, 45, 68, 15, 37, 15, 35, 45, 39, 87])
 b = np.array([45, 12, 456, 2341, 85654, 8567, 6854, 564, 98, 56, 7, 56, 8, 65, 7, 6, 7, 56, 8, 54, 5, 7, 4])
-c = np. array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0])
+c = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0])
+d = np.array([0, 1, 0, 0, 0, 0, 0, 0, 0])
 
-x = largest(a, 0, a.size-1)
-y = largest(b, 0, b.size-1)
-z = largest(c, 0, c.size-1)
 
+w = largest(a, 0, a.size-1)
+x = largest(b, 0, b.size-1)
+y = largest(c, 0, c.size-1)
+z = largest(d, 0, d.size-1)
+
+print(w)
 print(x)
 print(y)
 print(z)
