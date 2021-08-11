cbc1af6de07a181a607fc0bfb96e59266ca9b1e7
cator8 <cator8restart@gmail.com>
2021-08-11 13:47:20 -0400

    Added debug.py and helloworld.py

---
+++ lecture_notes/debug.py
@@ -0,0 +1,13 @@
+# Python program has errors
+var = "Double value"
+sumvalue = var + " " + str(4)
+
+
+def dosome(valuecheck):
+    if valuecheck > 4:
+        print("Bad Indent")
+
+
+dosome(5)
+
+print(sumvalue)
---
+++ lecture_notes/helloworld.py
@@ -0,0 +1,5 @@
+import json
+name = input("What's your name? ")
+print("Hello " + name + ".")
+age = input("How old are you? ")
+print(name + " is " + str(age) + " years old.")
